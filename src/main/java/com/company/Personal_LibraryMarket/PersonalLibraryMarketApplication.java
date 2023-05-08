f4cbd53169afce76fd732687c8f49cb0768f4097
BObidov <obidovbobur6768@gmail.com>
2023-05-08 16:38:34 +0500

    First commit Biz boshladik!

---
+++ src/main/java/com/company/Personal_LibraryMarket/PersonalLibraryMarketApplication.java
@@ -0,0 +1,13 @@
+package com.company.Personal_LibraryMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalLibraryMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalLibraryMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/company/Personal_LibraryMarket/PersonalLibraryMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.Personal_LibraryMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalLibraryMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
